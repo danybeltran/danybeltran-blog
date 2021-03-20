8bb45b3acb80a202ea9eb0046615e79690ec58c5
danybeltran <josuebeltran_2010@hotmail.com>
2021-03-19 23:33:51 -0600

    Remove secondary navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,16 +23,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Dany Beltran's blog</title>
       </Head>
-      <div className="px-4 py-12 h-screen">
-        <nav className="fixed shadow z-10 bg-white top-0 left-0 w-full space-x-2 h-12 flex items-center px-4 select-none">
-          <Link href="/">
-            <span className="font-semibold text-xl cursor-pointer">
-              Dany's blog
-            </span>
-          </Link>
-        </nav>
-        <Component {...pageProps} />
-      </div>
+      <Component {...pageProps} />
     </>
   );
 }
