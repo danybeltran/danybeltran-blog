45efb47ed28856254b40e4e6366c3f3b3c131588
danybeltran <josuebeltran_2010@hotmail.com>
2021-03-19 23:43:18 -0600

    fix too big story

--- pages/_document.js
+++ pages/_document.js
@@ -11,17 +11,8 @@ class MyDocument extends Document {
       <Html>
         <Head />
         <body>
-          <div className="px-4 py-12 h-screen">
-            <nav className="fixed shadow z-10 bg-white top-0 left-0 w-full space-x-2 h-12 flex items-center px-4 select-none">
-              <Link href="/">
-                <span className="font-semibold text-xl cursor-pointer">
-                  Dany's blog
-                </span>
-              </Link>
-            </nav>
-            <Main />
-            <NextScript />
-          </div>
+          <Main />
+          <NextScript />
         </body>
       </Html>
     );
