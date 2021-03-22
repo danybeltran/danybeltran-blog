634e6eaafc74460988154ed0e4ab0d83891c50d5
danybeltran <josuebeltran_2010@hotmail.com>
2021-03-21 22:33:42 -0600

    Change share button type to 'button' instead of 'button_count' as count is not acc.

--- src/components/ShareButton/index.tsx
+++ src/components/ShareButton/index.tsx
@@ -2,12 +2,9 @@ export default function ShareButton({ postUrl = "firt-post" }) {
   const shareUrl = encodeURIComponent(
     `https://blog.danybeltran.me/post/${postUrl}`
   );
-  console.log(
-    `https://www.facebook.com/plugins/share_button.php?href=${shareUrl}&layout=button&size=small&width=96&height=32&appId`
-  );
   return (
     <iframe
-      src={`https://www.facebook.com/plugins/share_button.php?href=${shareUrl}&layout=button_count&size=small&width=96&height=32&appId`}
+      src={`https://www.facebook.com/plugins/share_button.php?href=${shareUrl}&layout=button&size=small&width=96&height=32&appId`}
       width="96"
       height="20"
       style={{ border: "none", overflow: "hidden" }}
